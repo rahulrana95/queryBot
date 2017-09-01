000a525d4ad7be556ded650cdaf3486a96f4d5bb
lokesh <lokeshratusaria348@gmail.com>
2017-09-01 16:56:53 +0530

    testing loginverify API with password encryption

--- index.js
+++ index.js
@@ -74,28 +74,28 @@ var loginpost=router.post('/loginpost',function(req,res){
     var userName = req.body['username'];
     var password = req.body['password'];
     var timestamp = new Date().toISOString().slice(0, 19).replace('T', ' ');
-
     const saltRounds = 10;
     bcrypt.genSalt(saltRounds, function(err, salt) {
     bcrypt.hash(password, salt, function(err, hash) {
-        var q= `INSERT INTO users VALUES ('','${userName}','${hash}','${timestamp}');`;
-        connection.query(q,function(err,result){
-      });
-    });
 
-      if(err){
-        console.log(err);
-        res.json({
-          "status":"404",
-          "error":err
-        });
-      }
-      else {
-        res.json({
-          "status":"200",
-         "result":result
-        });
-      }
+            var q= `INSERT INTO users VALUES ('','${userName}','${hash}','${timestamp}');`;
+            connection.query(q,function(err,result){
+
+            if(err){
+              console.log(err);
+              res.json({
+                "status":"404",
+                "error":err
+              });
+            }
+            else {
+              res.json({
+                "status":"200",
+               "result":result
+              });
+            }
+          });
+      });
     });
 });
 app.use(loginpost);
