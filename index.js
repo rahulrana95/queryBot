e8a4f56837dada6e0dc7a77a81cd2e4869b5026f
lokesh <lokeshratusaria348@gmail.com>
2017-09-01 14:25:22 +0530

    testing loginpost API

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ var loginpost=router.post('/loginpost',function(req,res){
     var userName = req.body['username'];
     var password = req.body['password'];
 
-    var q= `INSERT INTO users VALUES ('','${userName}','${password}',GETDATE());`;
+    var q= `INSERT INTO users VALUES ('','${userName}','${password}','GETDATE()');`;
     connection.query(q,function(err,result){
 
       if(err){
