b72ce9439a39a6daf7cda5da3f92bb7b681a0daa
lokesh <lokeshratusaria348@gmail.com>
2017-09-01 17:46:10 +0530

    testing loginverify API with password encryption

--- index.js
+++ index.js
@@ -122,7 +122,7 @@ var loginverify=router.get('/loginverify',function(req,res){
       else {
         var check=true;
         for(var r in result){
-          bcrypt.compare(password, result[r].password, function(err, check) {
+          bcrypt.compare(password, result.password, function(err, check) {
               //check = 1
           });
         }
